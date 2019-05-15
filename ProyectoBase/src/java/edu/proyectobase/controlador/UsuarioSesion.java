5925259e69096abb8372a265fd16be8db3659c6c
Daniel Camilo Garay Cano <xcbdgara@intcomex.com>
2019-05-14 21:48:11 -0500

    Creación de roles e ingreso con usuario

--- ProyectoBase/src/java/edu/proyectobase/controlador/UsuarioSesion.java
+++ ProyectoBase/src/java/edu/proyectobase/controlador/UsuarioSesion.java
@@ -1,11 +1,14 @@
 package edu.proyectobase.controlador;
 
 import edu.proyectobase.modelo.Rol;
+import edu.proyectobase.modelo.Usuario;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 @Named(value = "usuarioSesion")
 @SessionScoped
@@ -16,7 +19,11 @@ public class UsuarioSesion implements Serializable {
     private String usuario = "";
     private String clave = "";
     private String correo = "";
+    private List<Usuario> usuarioRegistrados = new ArrayList<>();
 
+    public UsuarioSesion() {
+
+    }
 
     public String getNombre() {
         return nombre;
@@ -58,9 +65,59 @@ public class UsuarioSesion implements Serializable {
         this.correo = correo;
     }
 
-    
-    public UsuarioSesion() {
+    public List<Usuario> getUsuarioRegistrados() {
+        return usuarioRegistrados;
+    }
 
+    public void setusuarioRegistrados(List<Usuario> usuarioRegistrados) {
+        this.usuarioRegistrados = usuarioRegistrados;
+    }
+
+    public boolean ValidarNombreUsuario() {
+        for (Usuario obj : usuarioRegistrados) {
+            if (obj.getUsuario().equals(usuario)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String validarUsuario() {
+        for (Usuario obj : usuarioRegistrados) {
+            if (obj.getUsuario().equals(usuario) && obj.getClave().equals(clave)) {
+                return "Administrador/index.xhtml?faces-redirect=true";
+            }
+        }
+
+        FacesMessage inforMessagge = new FacesMessage(FacesMessage.SEVERITY_INFO, "Usuario no existe", "!");
+        FacesContext.getCurrentInstance().addMessage("mensajeNoExisteUser", inforMessagge);
+
+        return "";
+    }
+
+    public void adicionarUsuario() {
+        if (ValidarNombreUsuario()) {
+            List<Rol> listaMisRoles = new ArrayList<>();
+            Rol roles = new Rol("Adrministrador", "Todos los permisos");
+            listaMisRoles.add(roles);
+            Usuario usulog = new Usuario(nombre, apellido, usuario, clave, correo, listaMisRoles);
+            usuarioRegistrados.add(usulog);
+            LimpiarFormulario();
+            FacesMessage inforMessagge = new FacesMessage(FacesMessage.SEVERITY_INFO, "Usuario registrado", "satisfactoriamente");
+            FacesContext.getCurrentInstance().addMessage("mensajeRegistro", inforMessagge);
+        } else {
+            FacesMessage inforMessagge = new FacesMessage(FacesMessage.SEVERITY_INFO, "Usuario ya existente", "!!");
+            FacesContext.getCurrentInstance().addMessage("mensajeRegistro", inforMessagge);
+        }
+
+    }
+
+    public void LimpiarFormulario() {
+        this.nombre = "";
+        this.apellido = "";
+        this.usuario = "";
+        this.clave = "";
+        this.correo = "";
     }
 
 }
